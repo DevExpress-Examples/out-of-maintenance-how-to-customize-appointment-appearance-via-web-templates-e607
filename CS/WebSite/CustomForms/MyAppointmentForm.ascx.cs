d275cc3c29df52c09045a039c780132cb3990f5b
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 17:32:10 +0400

    Change example status to out of maintenance

--- CS/WebSite/CustomForms/MyAppointmentForm.ascx.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public partial class Templates_MyAppointmentForm : System.Web.UI.UserControl {
-	protected void Page_Load(object sender, EventArgs e) {
-
-	}
-}
--- VB/WebSite/CustomForms/MyAppointmentForm.ascx.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-
-Partial Public Class Templates_MyAppointmentForm
-    Inherits System.Web.UI.UserControl
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-
-    End Sub
-End Class
